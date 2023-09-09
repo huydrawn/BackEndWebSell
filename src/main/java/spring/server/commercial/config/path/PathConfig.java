5d1a5f477d6e2b55a465f5e69a91cd694f6509c3
huydrawn <huy112233778899@gmail.com>
2023-09-09 12:55:10 +0700

    update for refresh token

--- src/main/java/spring/server/commercial/config/path/PathConfig.java
+++ src/main/java/spring/server/commercial/config/path/PathConfig.java
@@ -2,6 +2,6 @@ package spring.server.commercial.config.path;
 
 public class PathConfig {
 	public static String[] getPathPermitAll() {
-		return new String[] {"/token/**","/product/**", "/register/**", "/verification/**", "/auth/**", "/swagger-ui.html/*", "/test/**" };
+		return new String[] {"/product/**", "/register/**", "/verification/**", "/auth/**", "/swagger-ui.html/*", "/test/**" };
 	}
 }
