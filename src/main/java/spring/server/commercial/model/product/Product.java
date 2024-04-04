544c447df3965f09f414140e4b694fb6baf19f01
QuanghuyDrawn <21130376@st.hcmuaf.edu.vn>
2024-04-04 09:35:58 +0700

    update  Product

--- src/main/java/spring/server/commercial/model/product/Product.java
+++ src/main/java/spring/server/commercial/model/product/Product.java
@@ -3,7 +3,6 @@ package spring.server.commercial.model.product;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -17,7 +16,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -25,7 +23,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import spring.server.commercial.model.Gender;
-import spring.server.commercial.model.cart.Cart;
 import spring.server.commercial.model.order.Order;
 import spring.server.commercial.model.user.Customer;
 
@@ -36,13 +33,12 @@ import spring.server.commercial.model.user.Customer;
 @Builder
 public class Product {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String describer;
 	private BigDecimal price;
-	@ManyToMany
-	List<Cart> carts;
+
 	@Enumerated
 	private Gender gender;
 	@ElementCollection
@@ -76,8 +72,7 @@ public class Product {
 			list.add(this);
 		}
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", describer=" + describer + ", price=" + price + ", gender="
